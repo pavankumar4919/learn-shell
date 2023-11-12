c38201ee0697488ee6b67cd19792e9d7b5060be4
Kota Ganesh <pavan.kota439@gmail.com>
2023-11-13 00:43:25 +0530

    catalogue changes

--- catalogue.sh
+++ catalogue.sh
@@ -1,25 +1,25 @@
-echo -e "\e[32m installing nginx \e[0m"
+echo -e "\e[32m Downloading from internet \e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-echo -e "\e[32m installing nginx \e[0m"
+echo -e "\e[32m installing nodejs \e[0m"
 dnf install nodejs -y
-echo -e "\e[32m installing nginx \e[0m"
+echo -e "\e[32m adding user \e[0m"
 useradd roboshop
-echo -e "\e[32m installing nginx \e[0m"
+echo -e "\e[32m creating directory \e[0m"
 mkdir /app
-echo -e "\e[32m installing nginx \e[0m"
+echo -e "\e[32m Downloading from internet \e[0m"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 cd /app
 unzip /tmp/catalogue.zip
 cd /app
 npm install
-echo -e "\e[32m installing nginx \e[0m"
+echo -e "\e[32m copying catalogue service \e[0m"
 cp configs/catalogue.service /etc/systemd/system/catalogue.service
-echo -e "\e[32m installing nginx \e[0m"
+echo -e "\e[32m reload service \e[0m"
 systemctl daemon-reload
-echo -e "\e[32m installing nginx \e[0m"
+echo -e "\e[32m copying catalogue service \e[0m"
 cp configs/catalogue.service /etc/yum.repos.d/mongo.repo
-echo -e "\e[32m installing nginx \e[0m"
+echo -e "\e[32m installing mangodb \e[0m"
 dnf install mongodb-org-shell -y
-echo -e "\e[32m installing nginx \e[0m"
+echo -e "\e[32m mangodbserver \e[0m"
 mongo --host mongodb-dev.pavandevops.com </app/schema/catalogue.js
 
