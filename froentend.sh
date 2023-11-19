f9c03b68a66cd84cf0e68572ebf8d564cf8ee270
Kota Ganesh <pavan.kota439@gmail.com>
2023-11-19 12:08:34 +0530

    repo changes

--- froentend.sh
+++ froentend.sh
@@ -5,7 +5,8 @@ rm -rf /usr/share/nginx/html/*
 echo -e "\e[32m Downloading the content from internet \e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 echo -e "\e[32m unzipping the file \e[0m"
-cd /usr/share/nginx/html unzip /tmp/frontend.zip
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
 echo -e "\e[32m transferring the repo \e[0m"
 cp configs/frontend.repo /etc/nginx/default.d/roboshop.conf
 echo -e "\e[32m restarting nginx \e[0m"
--- print.sh
+++ print.sh
@@ -2,4 +2,5 @@ echo hello world
 echo -e "\e[31m hello world \e[0m"
 echo hello world
 echo hello world
+x=10
 
