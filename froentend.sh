743238e6c612b1743a871a21055fda59d1bbb746
Kota Ganesh <pavan.kota439@gmail.com>
2023-11-22 00:28:57 +0530

    function changes

--- froentend.sh
+++ froentend.sh
@@ -2,19 +2,20 @@ copy_dir=$(pwd)
 log_file=/tmp/out
 rm -f ${log_file}
 print_head(){
-
+echo -e "\e[32m $1 \e[0m"
 }
-echo -e "\e[32m installing nginx \e[0m"
+print_head "installing nginx"
 dnf install nginx -y &>>${log_file}
-echo -e "\e[32m removing content \e[0m"
+print_head "removing HTML contnent"
 rm -rf /usr/share/nginx/html/* &>>${log_file}
-echo -e "\e[32m Downloading the content from internet \e[0m"
+print_head "Browsing content from internet"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
-echo -e "\e[32m unzipping the file \e[0m"
+print_head "unzipping the file"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-echo -e "\e[32m transferring the repo \e[0m"
+print_head "copying the file"
 cp ${copy_dir}/configs/frontend.repo /etc/nginx/default.d/roboshop.conf &>>${log_file}
-echo -e "\e[32m restarting nginx \e[0m"
+print_head "enabling the Nginx"
 systemctl enable nginx &>>${log_file}
+print_head "restarting the Nginx"
 systemctl restart nginx &>>${log_file}
