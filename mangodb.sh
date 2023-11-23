6ca923f657ea8b73388e01df0d74ca1b38e4c96e
Kota Ganesh <pavan.kota439@gmail.com>
2023-11-23 11:53:32 +0530

    function changes

--- mangodb.sh
+++ mangodb.sh
@@ -4,7 +4,7 @@ cp ${pwd}/configs/mango repo /etc/yum.repos.d/mongo.repo >>&${log_file}
 print_head "installing mangodb"
 dnf install mongodb-org -y >>&${log_file}
 print_head "enabling mangodb"
-systemctl enable mongod >>&&${log_file}
+systemctl enable mongod >>&${log_file}
 print_head "restarting mangodb"
-systemctl restart mongod >>&&${log_file}
+systemctl restart mongod >>&${log_file}
 
